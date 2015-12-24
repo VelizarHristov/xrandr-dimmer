ea1ba709500cfc81f0a5c2994592fee1df15341f
VelizarHristov <8lackened@gmail.com>
2015-12-24 23:55:54 +0000

    Fixed setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name         = 'xrandr dimmer',
+      version      = '1.0',
+      package_dir  = {'xrandr_dimmer': 'src/xrandr_dimmer'},
+      packages     = ['xrandr_dimmer'],
+      author       = 'Velizar Hristov',
+      author_email = 'velizar.hs@gmail.com',
+      license      = 'BSD-3',
+      url          = 'https://github.com/VelizarHristov/xrandr-dimmer',
+      description  = 'A GUI application for screen dimming with xrandr',
+#     long_description = """TODO""",
+      requires     = ["PyQt5 (>=5.3)"],
+      )
